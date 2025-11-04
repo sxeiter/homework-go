f818990084f841c7bd73e82c10063438fa28517b
Pavel Baksheev <sxeiter@mail.ru>
2025-11-04 13:02:18 +0500

    сделал генерацию id

--- bin/bin.go
+++ bin/bin.go
@@ -1,23 +1,36 @@
 package bin
 
-import "time"
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"time"
+)
 
 type Bin struct {
-	Id        string
-	Private   bool
-	CreatedAt time.Time
-	Name      string
+	Id        string    `json:"id"`
+	Private   bool      `json:"private"`
+	CreatedAt time.Time `json:"created_at"`
+	Name      string    `json:"name"`
 }
 
 type BinList struct {
 	Bins []Bin
 }
 
-func (b *Bin) NewBin() *Bin {
+func NewBin(name string, private bool) *Bin {
 	return &Bin{
-		Id:        b.Id,
-		Private:   b.Private,
-		CreatedAt: b.CreatedAt,
-		Name:      b.Name,
+		Id:        generateId(),
+		Private:   private,
+		CreatedAt: time.Now(),
+		Name:      name,
 	}
+}
+
+func generateId() string {
+	bytes := make([]byte, 8)
+	if _, err := rand.Read(bytes); err != nil {
+		return fmt.Sprintf("bin_%d", time.Now().UnixNano())
+	}
+	return "bin_" + hex.EncodeToString(bytes)
 }
